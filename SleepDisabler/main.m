d393ebd602ff5ee33a00340796eb3c42e79bde6a
Renjith <renjith@localhost.(none)>
2012-10-31 21:35:18 +0530

    Initial Commit

---
+++ SleepDisabler/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SleepDisabler
+//
+//  Created by Renjith on 31/10/12.
+//  Copyright (c) 2012 Renjith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
